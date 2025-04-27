ae385951e196120e658f0fc3e48ef20c5594e721
theamik <amik.sylhet@gmail.com>
2025-04-28 00:37:40 +0600

    db

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 module.exports.dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.DB_LOC_URL);
+    await mongoose.connect(process.env.DB_LOC_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`database connected with ${process.env.DB_LOC_URL}`);
   } catch (error) {
     console.log(error.message);
