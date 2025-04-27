bf8f4198dcb60fbb9009d08cc64af4763136788f
theamik <amik.sylhet@gmail.com>
2025-04-27 20:07:37 +0600

    back

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,24 +1,11 @@
-import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
-module.exports = defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig({
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "lib/main.js"),
-      name: "MyLib",
-      fileName: (format) => `my-lib.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
+    outDir: "build",
   },
+  plugins: [react(), svgr()],
 });
