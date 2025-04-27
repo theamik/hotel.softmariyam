56a97d177684ad4cab009ad405823b4c1e829b0d
theamik <amik.sylhet@gmail.com>
2025-04-27 23:54:10 +0600

    dist

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
-  plugins: [react(), svgr()],
+  build: { manifest: true, outDir: "./dist" },
+  base: "/",
+  root: "./src",
+  plugins: [react()],
 });
