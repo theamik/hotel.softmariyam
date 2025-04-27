80a6d0fe986d9fb9e53c27b98e9978c58cc0d403
theamik <amik.sylhet@gmail.com>
2025-04-27 19:58:04 +0600

    viteconfig

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
-  plugins: [react(), svgr()],
+  plugins: [react()],
 });
