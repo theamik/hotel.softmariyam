d4a084bb6861ecf960b9c219ad01b40cf5e65821
theamik <amik.sylhet@gmail.com>
2025-04-28 00:10:52 +0600

    test

--- server.js
+++ server.js
@@ -36,21 +36,21 @@ exports.handler = async (event, context) => {
   });
 };
 
-// app.use(
-//   cors({
-//     origin: ["https://hotel-softmariyam.vercel.app"],
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
-
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://hotel-softmariyam.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173"],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 
 app.use(bodyParser.json());
 app.use(cookieParser());
