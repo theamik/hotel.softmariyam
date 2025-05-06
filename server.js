663d7fc8873d65cd765160173ecae36d2a7ab9d6
theamik <amik.sylhet@gmail.com>
2025-05-06 15:02:52 +0600

    api update

--- server.js
+++ server.js
@@ -11,21 +11,21 @@ const morgan = require("morgan");
 require("dotenv").config();
 const server = http.createServer(app);
 
-// app.use(
-//   cors({
-//     origin: ["https://hotel-softmariyam.vercel.app"],
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
-
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://hotel-softmariyam.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173"],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 app.use(express.json({ limit: "10mb" }));
 app.use(
   bodyParser.urlencoded({
